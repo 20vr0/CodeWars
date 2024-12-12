c0a3bac49c05447d91e626a217ddb0d16866d63f
20vr0 <191410146+20vr0@users.noreply.github.com>
2024-12-12 07:58:44 +0000

    Update Opposite Number.cs

--- 8 kyu/Opposite Number.cs
+++ 8 kyu/Opposite Number.cs
@@ -1,3 +1,4 @@
+//My solution
 using System;
 
 public class Kata
@@ -12,5 +13,26 @@ public class Kata
           {
            return number = number + (-2*number); 
           }
+        }
+    }
+
+//Optimal solutions
+using System;
+
+public class Kata
+    {
+        public static int Opposite(int number)
+        {
+            return -number;
+        }
+    }
+
+using System;
+
+public class Kata
+    {
+        public static int Opposite(int number)
+        {
+           return number * -1;
         }
     }
